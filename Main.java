12fb038dd90fd66d26e6365a24ba99de59b84b59
MD. Mehedi Hasan <167318121+meetmehedi@users.noreply.github.com>
2025-01-21 00:24:16 +0600

    Add files via upload
    Multilevel Inheritance
    
    Objective
    
    To design a system for managing faculty, students, and departments using multilevel inheritance.
    
    Description/Introduction
    
    The program demonstrates multilevel inheritance by creating a base class Person, subclass Faculty, and another subclass Professor, each adding specific attributes and behaviors.
    
    Method
    	1.	Define Person class: Add attributes and displayInfo() method.
    	2.	Create Faculty subclass: Extend Person and add faculty-specific details.
    	3.	Create Professor subclass: Extend Faculty and add research details.
    	4.	Test in main: Create and display a Professor object.
    
    Conclusion
    
    This program illustrates multilevel inheritance, enabling hierarchical organization and extended functionality at each level.

--- Main.java
+++ Main.java
@@ -1,17 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    Manager manager = new Manager();
-    manager.setEmployed("M04");
-    manager.setName("Mehedi");
-    manager.setDepartment("R&D");
-    manager.setBonus(2000);
-   double salary = manager.calculateTotalSalary(7000);
-        System.out.println("Manager details: ");
-        System.out.println("Name: " + manager.getName());
-        System.out.println("Department: " + manager.getDepartment());
-        System.out.println("ID: " + manager.getEmployed());
-        System.out.println("Salary: " + salary);
+        Professor professor = new Professor();
+        professor.setName("Abdul Based");
+        professor.setAge(45);
+        professor.setFacultyId("B02");
+        professor.setDepartment("Computer Science");
+        professor.setResearchArea("Artificial Intelligence");
+        professor.displayInfo();
     }
 }
